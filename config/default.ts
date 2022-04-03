6990377ced3a3152daf67015e43acf293f67d079
hatim <hatimmouline2014@gmail.com>
2022-04-03 22:26:41 +0000

    first commit

---
+++ config/default.ts
@@ -0,0 +1,5 @@
+export default {
+  port: 5001,
+  dbUri:
+  "mongodb://moulinehatim:3kBQXe1fVEAqQMpp@cluster0-shard-00-00.botec.mongodb.net:27017,cluster0-shard-00-01.botec.mongodb.net:27017,cluster0-shard-00-02.botec.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-422nht-shard-0&authSource=admin&retryWrites=true&w=majority",
+};
